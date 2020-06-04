f1d41ede8cd00fea37c1a365f852bc74f86a427a
Peng Mei Yu <pengmyu@gmail.com>
2020-06-04 13:34:08 +0800

    guix: channel: Use local repositories.

--- guix/channels.scm
+++ guix/channels.scm
@@ -3,15 +3,21 @@
 ;;; Default:
 ;; %default-channels
 
-(cons* (channel
-        (name 'my-packages)
-        (url "https://github.com/pmeiyu/guix-packages.git"))
-       %default-channels)
+;; (cons* (channel
+;;         (name 'my-packages)
+;;         (url "https://github.com/pmeiyu/guix-packages.git"))
+;;        %default-channels)
 
-;; (list (channel
-;;        (name 'guix)
-;;        (branch "dev")
-;;        (url "https://github.com/pmeiyu/guix.git"))
-;;       (channel
-;;        (name 'my-packages)
-;;        (url "https://github.com/pmeiyu/guix-packages.git")))
+
+(define (project-directory project)
+  (string-append (or (getenv "HOME")
+                     (passwd:dir (getpwuid (getuid))))
+                 "/Projects/"
+                 project))
+
+(list (channel
+       (name 'guix)
+       (url (project-directory "guix")))
+      (channel
+       (name 'my-packages)
+       (url (project-directory "guix-packages"))))
