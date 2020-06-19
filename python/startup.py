20e4709e42778e8440766e6ee4f3d76e1b3b72c8
Peng Mei Yu <pengmyu@gmail.com>
2020-06-19 09:21:14 +0800

    python: startup: Import more modules.

--- python/startup.py
+++ python/startup.py
@@ -1,10 +1,17 @@
+import cProfile
 import datetime
 import functools
+import http
+import io
 import itertools
 import json
 import os
+import pdb
 import sys
 import time
+import timeit
+import trace
+import tracemalloc
 import urllib
 
 from functools import reduce
