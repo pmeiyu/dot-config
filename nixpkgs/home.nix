c1446ed38fc4e13d0255f9ca32bc397701d725e2
Peng Mei Yu <i@pengmeiyu.com>
2021-04-28 12:40:44 +0800

    nixpkgs: home-manager: Add firefox.

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -15,4 +15,22 @@
     programming-packages-tools
     programming-packages-python
   ];
+
+  programs.firefox = {
+    enable = true;
+    package = pkgs.firefox-esr;
+    profiles = {
+      default = {
+        id = 0;
+        settings = {
+          "browser.aboutConfig.showWarning" = false;
+          "browser.tabs.closeWindowWithLastTab" = false;
+          "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
+          "browser.newtabpage.activity-stream.feeds.snippets" = false;
+          "signon.rememberSignons" = false;
+          "ui.key.menuAccessKeyFocuses" = false;
+        };
+      };
+    };
+  };
 }
