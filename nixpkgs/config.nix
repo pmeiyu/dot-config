4474e7de007f26f3e20c3a4c74d864ad8959ca1e
Peng Mei Yu <pmy@xqzp.net>
2021-10-22 11:59:43 +0800

    nixpkgs: Delete packageOverrides.

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,11 +1,4 @@
 with (import <nixpkgs> { }); {
   # allowBroken = true;
   # allowUnfree = true;
-
-  packageOverrides = pkgs: {
-    local-packages = pkgs.buildEnv {
-      name = "local-packages";
-      paths = with pkgs; [ ];
-    };
-  };
 }
