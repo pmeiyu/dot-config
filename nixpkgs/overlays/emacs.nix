177d40a2de280592885551e76f4fc3d3e91f5bfd
Peng Mei Yu <pmy@xqzp.net>
2021-11-22 17:51:52 +0800

    nixpkgs: overlays: emacs: Only import necessary packages.
    Prevent downloading tarball for unrelated evaluation.

--- nixpkgs/overlays/emacs.nix
+++ nixpkgs/overlays/emacs.nix
@@ -1,3 +1,15 @@
-import (builtins.fetchTarball {
-  url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
-})
+self: super:
+
+let
+  overlay = import (builtins.fetchTarball {
+    url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+  });
+  pkgs = overlay self super;
+in
+{
+  emacsGit = pkgs.emacsGit;
+  emacsGit-nox = pkgs.emacsGit-nox;
+  emacsPgtk = pkgs.emacsPgtk;
+  emacsUnstable = pkgs.emacsUnstable;
+  emacsUnstable-nox = pkgs.emacsUnstable-nox;
+}
